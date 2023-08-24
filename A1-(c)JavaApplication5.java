f66b0ffc1648fa7bc79081a3cf81c15026fd5a79
05DhakshaM <121370028+05DhakshaM@users.noreply.github.com>
2023-08-24 21:01:15 +0530

    Add files via upload

---
+++ A1-(c)JavaApplication5.java
@@ -0,0 +1,9 @@
+package javaapplication5;
+
+public class Randomnumber {
+    public static void main(String[] args){
+        for(int i = 0; i < 5; i++){
+            System.out.println((int)(Math.random() * 100));
+        }
+    }
+}
